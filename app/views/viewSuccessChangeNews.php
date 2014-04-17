0fee5ca405c5fa98d4bcbdf2774ceb8d61d74ddb
root <root@php-System-Product-Name.(none)>
2014-04-17 12:58:14 +0300

    first commit

---
+++ app/views/viewSuccessChangeNews.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Success add TAG</title>
+</head>
+<body>
+    	<div class="welcome">
+            
+            <div>
+            </div>
+            	<h1>You changes add in News!!</h1>
+<?php echo View::make('Link'); ?>
+	</div>
+</body>
+</html>
