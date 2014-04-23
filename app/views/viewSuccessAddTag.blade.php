ad57b9998015a1a84cc939e1a24d94454b3bd9a9
root <root@php-System-Product-Name.(none)>
2014-04-23 10:41:17 +0300

    add blade file viewSuccessAddTag

---
+++ app/views/viewSuccessAddTag.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Success add TAG</title>
+</head>
+<body>
+    	<div class="welcome">
+            
+            <div>
+            </div>
+            	<h1>You new TAG add successes!!</h1>
+{{View::make('Link');}}
+	</div>
+</body>
+</html>
