09b3e2c03f1d877fd3b4c00fe8eba879cc120be6
root <root@php-System-Product-Name.(none)>
2014-04-23 10:40:06 +0300

    add blade file viewSuccessAddReview

---
+++ app/views/viewSuccessAddReview.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Success add Review</title>
+</head>
+<body>
+    	<div class="welcome">
+            
+            <div>
+            </div>
+            	<h1>You new Review add successes!!</h1>
+{{View::make('Link'); }}
+	</div>
+</body>
+</html>
