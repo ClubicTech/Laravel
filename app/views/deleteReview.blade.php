a29937bdfe89ff99aac1c382eb9af5c32a16447d
root <root@php-System-Product-Name.(none)>
2014-04-23 10:16:17 +0300

    add delete Review blade file

---
+++ app/views/deleteReview.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Success DELETED News</title>
+</head>
+<body>
+    	<div class="welcome">
+            
+            <div>
+            </div>
+            	<h1>You Review DELETE successes!!</h1>
+{{ View::make('Link');}}
+        </div>
+</body>
+</html>
