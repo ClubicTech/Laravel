003eca2131fc24d6ea45387598eb4a0533c34bd3
root <root@php-System-Product-Name.(none)>
2014-04-23 10:39:39 +0300

    add blade file viewSuccessAddNews

---
+++ app/views/viewSuccessAddNews.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Success add News</title>
+</head>
+<body>
+    	<div class="welcome">
+            
+            <div>
+            </div>
+            	<h1>You new News add successes!!</h1>
+{{View::make('Link');}}
+	</div>
+</body>
+</html>
