634683312f3da6d189c53b418f80812bcaa3f47c
root <root@php-System-Product-Name.(none)>
2014-04-29 12:28:46 +0300

    add news link

--- app/views/layouts/link.blade.php
+++ app/views/layouts/link.blade.php
@@ -1,4 +1,4 @@
-                <a href="http://test1.com" title="return to home">Return to home?</a><br>
+                <a href="http://test1.com">Return to home?</a><br>
                 <a href="http://test1.com/news" > Show news list </a><br>
                 <a href="http://test1.com/review" > Show Reviews </a><br>
                 <a href="http://test1.com/rubric" > Show Rubric </a><br>
@@ -10,3 +10,7 @@
                 <a href="http://test1.com/delete-news" > Delete News</a><br>
                 <a href="http://test1.com/delete-review" > Delete Review</a><br>
                 <a href="http://test1.com/ajax-add-news" > ajax-add-news</a><br>
+                <a href="http://test1.com/get-all-api-news" > get-all-api-news </a><br>
+                <a href="http://test1.com/news-api-search" > news-api-search </a><br>
+                <a href="http://test1.com/review-news-api-search" > review-news-api-search </a><br>
+                <a href="http://test1.com/create-news-api" > create-news-api </a><br>
