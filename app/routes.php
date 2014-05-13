ac9105ff5ff1f8a012a1d3644f3c766723f1a781
root <root@php-System-Product-Name.(none)>
2014-05-13 12:07:54 +0300

    delete coment

--- app/routes.php
+++ app/routes.php
@@ -44,77 +44,64 @@ Route::post('login', 'AuthController@setLogin');
 Route::get('registration', 'AuthController@registrationForm');
 Route::post('registration', 'AuthController@registrationUser');
 
+Route::get('logout', 'AuthController@logout');
 
+Route::get('change-news-api/{id}', 'RequestAPIController@formMakeChanges');
+Route::post('change-news-api/{id}', 'RequestAPIController@savingChangesNews');
 
-//Route::group(array('before' => 'auth'), function () {
-    
-    Route::get('logout', 'AuthController@logout');
-    
-    Route::get('change-news-api/{id}', 'RequestAPIController@formMakeChanges');
-    Route::post('change-news-api/{id}', 'RequestAPIController@savingChangesNews');
+Route::get('get-all-api-news', 'RequestAPIController@showAllNews');
+Route::get('one-news-api/{id}', 'RequestAPIController@displayOneNews');
 
-    Route::get('get-all-api-news', 'RequestAPIController@showAllNews');
-    Route::get('one-news-api/{id}', 'RequestAPIController@displayOneNews');
+Route::get('news-api-search', 'RequestAPIController@formSearchNews');
+Route::post('news-api-search', 'RequestAPIController@showSearchNews');
 
-    Route::get('news-api-search', 'RequestAPIController@formSearchNews');
-    Route::post('news-api-search', 'RequestAPIController@showSearchNews');
+Route::get('review-news-api-search', 'RequestAPIController@formSearchNewsAndReview');
+Route::post('review-news-api-search', 'RequestAPIController@showSearchNewsAndReview');
 
-    Route::get('review-news-api-search', 'RequestAPIController@formSearchNewsAndReview');
-    Route::post('review-news-api-search', 'RequestAPIController@showSearchNewsAndReview');
+Route::get('delete-news-api/{id}', 'RequestAPIController@removalNews');
 
-    Route::get('delete-news-api/{id}', 'RequestAPIController@removalNews');
+Route::get('create-news-api', 'RequestAPIController@formCreatingNews');
+Route::post('create-news-api', 'RequestAPIController@addingCreatedNews');
 
-    Route::get('create-news-api', 'RequestAPIController@formCreatingNews');
-    Route::post('create-news-api', 'RequestAPIController@addingCreatedNews');
+Route::get('api.test1/news/delete/{id}', 'NewsController@deteteJSONNews');
+Route::delete('api.test1/news/delete/{id}', 'NewsController@deteteJSONNews');
 
-    Route::get('api.test1/news/delete/{id}', 'NewsController@deteteJSONNews');
-    Route::delete('api.test1/news/delete/{id}', 'NewsController@deteteJSONNews');
+Route::post('api.test1/news/change/{id}', 'NewsController@changeJSONNews');
 
-    Route::post('api.test1/news/change/{id}', 'NewsController@changeJSONNews');
+Route::post('api.test1/news/create', 'NewsController@createJSONNews');
 
-    Route::post('api.test1/news/create', 'NewsController@createJSONNews');
+Route::get('add-review', 'ReviewController@formAddReview');
+Route::post('add-review', 'ReviewController@addReview');
 
-    Route::get('add-review', 'ReviewController@formAddReview');
-    Route::post('add-review', 'ReviewController@addReview');
+Route::get('add-rubric', 'RubricController@formAddRubric');
+Route::post('add-rubric', 'RubricController@addRubric');
 
-    Route::get('add-rubric', 'RubricController@formAddRubric');
-    Route::post('add-rubric', 'RubricController@addRubric');
+Route::get('add-tag', 'TagController@formAddTag');
+Route::post('add-tag', 'TagController@addTag');
 
-    Route::get('add-tag', 'TagController@formAddTag');
-    Route::post('add-tag', 'TagController@addTag');
+Route::get('delete-news', 'NewsController@listDeleteNews');
+Route::post('delete-news', 'NewsController@deleteNews');
 
-    Route::get('delete-news', 'NewsController@listDeleteNews');
-    Route::post('delete-news', 'NewsController@deleteNews');
+Route::get('delete-review', 'ReviewController@listDeleteReview');
+Route::post('delete-review', 'ReviewController@deleteReview');
 
-    Route::get('delete-review', 'ReviewController@listDeleteReview');
-    Route::post('delete-review', 'ReviewController@deleteReview');
+Route::model('news', 'News');
+Route::get('change-news/{news}', "NewsController@changeNews");
+Route::post('change-news', 'NewsController@addChangeNews');
 
-    Route::model('news', 'News');
-    Route::get('change-news/{news}', "NewsController@changeNews");
-    Route::post('change-news', 'NewsController@addChangeNews');
+Route::get('ajax-change-news/{news}', 'AjaxController@changeNews');
 
-    Route::get('ajax-change-news/{news}', 'AjaxController@changeNews');
-//Route::post('ajax-change/{news}', 'AjaxController@ajaxAddChangeNews');
+Route::post('ajax-change-news/ajax-change', array('as' => 'ajax-change',
+    'ajax-change-news' => function () {
+echo AjaxController::ajaxAddChangeNews();
+}
+));
 
-    Route::post('ajax-change-news/ajax-change', array('as' => 'ajax-change',
-        'ajax-change-news' => function () {
-                                        echo AjaxController::ajaxAddChangeNews();
-                               }
-    ));
-
-    Route::get('ajax-add-news', 'AjaxController@ajaxAddNews');
-    Route::post('ajax-add-news', "AjaxController@ajaxAdd");
-//});
+Route::get('ajax-add-news', 'AjaxController@ajaxAddNews');
+Route::post('ajax-add-news', "AjaxController@ajaxAdd");
 
 
 
-
-// ---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||
-// ---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||
-// ---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||
-// ---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||
-//_________________________________________________________________________
-//_________________________________________________________________________
 
 Route::post('api.test1.api-login', 'UserAPIController@login');
 
@@ -124,40 +111,22 @@ Route::post('api.test1.api-islogged', 'UserAPIController@isLogged');
 
 Route::post('api.test1.api-logout', 'UserAPIController@logout');
 
-//_________________________________________________________________________
-//_________________________________________________________________________
-//
-// ---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||
-// ---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||
-// ---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||
-// ---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||
-// ---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||
-// ---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||
-
-
-
 
 
 //Route::post('api.test1.api/news', 'APINewsController@allNews');
 //
-//Route::post('api.test1.api/one-news', 'APINewsController@oneNews');
+    //Route::post('api.test1.api/one-news', 'APINewsController@oneNews');
 //
-//Route::post('api.test1.api/news/change', 'APINewsController@changeNews');
+    //Route::post('api.test1.api/news/change', 'APINewsController@changeNews');
 //
-//Route::post('api.test1.api/news/delete', 'APINewsController@deteteNews');
+    //Route::post('api.test1.api/news/delete', 'APINewsController@deteteNews');
 //
-//Route::post('api.test1.api/news/create', 'APINewsController@createNews');
+    //Route::post('api.test1.api/news/create', 'APINewsController@createNews');
 //
-//Route::post('api.test1.api/news/search', 'APINewsController@searchNews');
+    //Route::post('api.test1.api/news/search', 'APINewsController@searchNews');
 //
-//Route::post('api.test1.api/news/tag-search', 'APINewsController@tagSearchNews');
+    //Route::post('api.test1.api/news/tag-search', 'APINewsController@tagSearchNews');
 
-
-
-// ---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||
-// ---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||
-// ---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||
-// ---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||---|||
 
 Route::get('api-login', 'APIAuthComtroller@apiloginForm');
 
